ab70deb5cc1b22ba5f0ebe59098449622ba23e2b
yuji01 <59014140+yuji01@users.noreply.github.com>
2022-07-11 13:23:41 +0800

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -37,15 +37,17 @@ register_acme(){
 80_acme(){
   echo -e "${RED}请确保80端口没有被占用$END"
   read -p "请输入域名：" input
-  $DIR/.acme.sh/acme.sh  --issue -d $input --standalone &&
-  echo -e "${GREEN}域名：$input 的证书申请成功$END" ||
-  echo -e "${RED}域名：$input 的证书申请失败$END"
-  [ -d $SSL/$input ] || mkdir -p $SSL/$input
-  $DIR/.acme.sh/acme.sh --installcert -d $input --fullchain-file $SSL/$input/cret.crt --key-file $SSL/$input/private.key &&
+  $DIR/.acme.sh/acme.sh  --issue -d $input --standalone
+  if [ $? -eq 0 ];then
+    echo -e "${GREEN}域名：$input 的证书申请成功$END" 
+    [ -d $SSL/$input ] || mkdir -p $SSL/$input
+    $DIR/.acme.sh/acme.sh --installcert -d $input --fullchain-file $SSL/$input/cret.crt --key-file $SSL/$input/private.key &&
   echo -e "证书安装路径：
   证书公钥路径：${GREEN}$SSL/$input/cret.crt$END
-  证书私钥路径：${RED}$SSL/$input/private.key$END
-  "
+  证书私钥路径：${RED}$SSL/$input/private.key$END"
+  else
+    echo -e "${RED}域名：$input 的证书申请失败$END"
+  fi
 }
 cf_api(){
   echo -e "${RED}请确保cloudflare的Global API Key是生效的$END"
@@ -54,16 +56,17 @@ cf_api(){
   read -p "请输入cloudflare的邮箱：" email
   export CF_Email="$email"
   read -p "请输入你要申请的域名：" domain
-  ~/.acme.sh/acme.sh --issue --dns dns_cf -d 域名 -d *.域名 -k ec-256
   $DIR/.acme.sh/acme.sh --issue --dns dns_cf -d ${domain} -d *.${domain} -k ec-256 &&
-  echo -e "${GREEN}域名：$domain 的证书申请成功$END" ||
-  echo -e "${RED}域名：$domain 的证书申请失败$END"
-  [ -d $SSL/$domain ] || mkdir -p $SSL/$domain
-  $DIR/.acme.sh/acme.sh --installcert -d $domain -d *.${domain} --fullchainpath $SSL/$domain/$domain.crt --keypath $SSL/$domain/$domain.key –ecc &&
+  if [ $? -eq 0 ];then
+    echo -e "${GREEN}域名：$domain 的证书申请成功$END" 
+    [ -d $SSL/$domain ] || mkdir -p $SSL/$domain
+    $DIR/.acme.sh/acme.sh --installcert -d $domain -d *.${domain} --fullchainpath $SSL/$domain/$domain.crt --keypath $SSL/$domain/$domain.key –ecc &&
   echo -e "证书安装路径：
   证书公钥路径：${GREEN}$SSL/$domain/$domain.crt$END
-  证书私钥路径：${RED}$SSL/$domain/$domain.key$END
-  "
+  证书私钥路径：${RED}$SSL/$domain/$domain.key$END"
+  else
+    echo -e "${RED}域名：$domain 的证书申请失败$END"
+  fi 
 }
 update_acme(){
   $DIR/.acme.sh/acme.sh  --upgrade  --auto-upgrade && echo -e "${GREEN}自动更新设置完成$END"
