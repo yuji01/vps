67702d45b2a07dbe4b6b773460b558ef6c6efb83
yuji01 <59014140+yuji01@users.noreply.github.com>
2022-09-18 22:21:01 +0800

    Add files via upload

--- auto-trojan_go.sh
+++ auto-trojan_go.sh
@@ -44,14 +44,6 @@ make && make install && echo "succeed"
 cat << EOF
 #user  nobody;
 worker_processes  auto;
-
-#error_log  logs/error.log;
-#error_log  logs/error.log  notice;
-#error_log  logs/error.log  info;
-
-#pid        logs/nginx.pid;
-
-
 events {
     worker_connections  1024;
 }
@@ -63,7 +55,6 @@ stream {
         # 域名都不匹配情况下的默认值
         default web;
     }
-
     # web，配置转发详情
     upstream web {
         server 127.0.0.1:4431;
@@ -72,7 +63,6 @@ stream {
     upstream trojan {
         server 127.0.0.1:4432;
     }
-
     # 监听 443 并开启 ssl_preread
     server {
         listen 443 reuseport;
@@ -81,112 +71,17 @@ stream {
         ssl_preread on;
     }
 }
-
 http {
     include       web.conf;
-#   include       trojan.conf;
     include       mime.types;
     default_type  application/octet-stream;
-    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-    #                  '$status $body_bytes_sent "$http_referer" '
-    #                  '"$http_user_agent" "$http_x_forwarded_for"';
-
-    #access_log  logs/access.log  main;
-
     sendfile        on;
-    #tcp_nopush     on;
-
-    #keepalive_timeout  0;
     keepalive_timeout  65;
-
-    #gzip  on;
-#    server {
-#        listen       80;
-#        server_name  localhost;
-
-        #charset koi8-r;
-
-        #access_log  logs/host.access.log  main;
-
-#        location / {
-#            root   html;
-#            index  index.html index.htm;
-#        }
-
-        #error_page  404              /404.html;
-
-        # redirect server error pages to the static page /50x.html
-        #
-#        error_page   500 502 503 504  /50x.html;
-#        location = /50x.html {
-#            root   html;
-#        }
-
-        # proxy the PHP scripts to Apache listening on 127.0.0.1:80
-        #
-        #location ~ \.php$ {
-        #    proxy_pass   http://127.0.0.1;
-        #}
-
-        # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-        #
-        #location ~ \.php$ {
-        #    root           html;
-        #    fastcgi_pass   127.0.0.1:9000;
-        #    fastcgi_index  index.php;
-        #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
-        #    include        fastcgi_params;
-        #}
-
-        # deny access to .htaccess files, if Apache's document root
-        # concurs with nginx's one
-        #
-        #location ~ /\.ht {
-        #    deny  all;
-        #}
-#    }
-
-
-    # another virtual host using mix of IP-, name-, and port-based configuration
-    #
-    #server {
-    #    listen       8000;
-    #    listen       somename:8080;
-    #    server_name  somename  alias  another.alias;
-
-    #    location / {
-    #        root   html;
-    #        index  index.html index.htm;
-    #    }
-    #}
-
-
-    # HTTPS server
-    #
-    #server {
-    #    listen       443 ssl;
-    #    server_name  localhost;
-
-    #    ssl_certificate      cert.pem;
-    #    ssl_certificate_key  cert.key;
-
-    #    ssl_session_cache    shared:SSL:1m;
-    #    ssl_session_timeout  5m;
-
-    #    ssl_ciphers  HIGH:!aNULL:!MD5;
-    #    ssl_prefer_server_ciphers  on;
-
-    #    location / {
-    #        root   html;
-    #        index  index.html index.htm;
-    #    }
-    #}
-
 }
 EOF
 ) > /usr/local/nginx/conf/nginx.conf
-sed -i "17s/www.yuji2022.com/$WEB/g" /usr/local/nginx/conf/nginx.conf
-sed -i "18s/trojan.yuji2022.com/$TROJAN_GO/g" /usr/local/nginx/conf/nginx.conf
+sed -i "9s/www.yuji2022.com/$WEB/g" /usr/local/nginx/conf/nginx.conf
+sed -i "10s/trojan.yuji2022.com/$TROJAN_GO/g" /usr/local/nginx/conf/nginx.conf
 #web配置
 (
 cat << EOF
@@ -242,8 +137,8 @@ systemctl daemon-reload && systemctl start nginx.service && systemctl enable ngi
 }
 trojan(){
 cd /usr/local/
-wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
-unzip -d /usr/local/trojan-go/ trojan-go-linux-amd64.zip
+wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip &&
+unzip -d /usr/local/trojan-go/ trojan-go-linux-amd64.zip &&
 #trojan-go配置文件
 (
 cat << EOF
