65229ba3e3e2d9a383f620128da4177bffedc634
yuji01 <59014140+yuji01@users.noreply.github.com>
2023-02-18 22:17:32 +0800

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -19,16 +19,21 @@ else
   apt install build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
 fi
 #安装主体
-wget https://www.python.org/ftp/python/3.11.2/Python-3.11.2.tgz
-tar -zxf Python-3.11.2.tgz && rm -rf Python-3.11.2.tgz
-cd Python-3.11.2
-./configure --prefix=/usr/local/python311
+echo -e "\e[1;33m请输入你要下载的python版本\e[0m:
+例如：
+\e[1;31m3.11.2  3.10.3  3.9.13  3.8.13  3.7.10\e[0m"
+read -e -p "请输入：" version
+wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+[ ! $? -eq 0 ] && echo -e "\e[1;31m请输入正确的python版本!!!\e[0m" && exit 1
+tar -zxf Python-$version.tgz && rm -rf Python-$version.tgz
+cd Python-$version
+./configure --prefix=/usr/local/python$version
 make
 make install
 if [ $? -eq 0 ];then
   clear
   echo -e "\e[1;32m安装成功！\e[0m"
-  ln -s /usr/local/python311 /usr/local/bin/python3
+  ln -s /usr/local/python$version /usr/local/bin/python3
   echo "python 版本为："
   python3 -V
 else
