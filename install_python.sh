5d0749e68104cbbc6c486daf452499ded972e05a
yuji01 <59014140+yuji01@users.noreply.github.com>
2023-02-18 20:25:51 +0800

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -1,11 +1,27 @@
 #安装python
 
-apt update && apt install -y wget
+apt update && apt install -y wget make build-essential
+
 wget https://www.python.org/ftp/python/3.11.2/Python-3.11.2.tgz
 tar -zxf Python-3.11.2.tgz && rm -rf Python-3.11.2.tgz
 cd Python-3.11.2
-./configure --prefix=/usr/local/python311 &&
-make && echo "编译成功" || {echo "编译失败" , exit 1}
-make install && echo "安装成功" || { echo "安装失败" , exit 1 }
+./configure --prefix=/usr/local/python311
+make
+if [ $? -eq 0 ];then
+  clear
+  echo "编译成功"
+else
+  clear
+  echo "编译失败" && exit 1
+fi
+make install
+if [ $? -eq 0 ];then
+  clear
+  echo "安装成功"
+else
+  clear
+  echo "安装失败" && exit 1
+fi
 ln -s /usr/local/python311 /usr/local/bin/python3
-echo "完成"
+echo "python 版本为："
+python3 -V
