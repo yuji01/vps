bff1bce9455a2b54b17880073614c9c65a422779
yuji01 <59014140+yuji01@users.noreply.github.com>
2023-03-05 14:18:36 +0800

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
-#设置虚拟内存
+# 设置虚拟内存
 [ -d /usr/swap ] || mkdir /usr/swap
-read -e -p "输入虚拟内存大小：" size
+read -e -p "输入虚拟内存大小（M）：" size
 dd if=/dev/zero of=/usr/swap/swapfile bs=1M count=$size &&
 du -sh /usr/swap/swapfile &&
-#格式化为swap
+# 格式化为swap
 mkswap /usr/swap/swapfile &&
-#挂载swap
+# 设置权限
+chmod 0600 /usr/swap/swapfile &&
+# 挂载swap
 swapon /usr/swap/swapfile &&
-#查看内存
+# 查看内存
 free -m
-cat '/usr/swap/swapfile swap swap defaults 0 0' >> /etc/fstab
+# 设置开机自启
+echo '/usr/swap/swapfile swap swap defaults 0 0' >> /etc/fstab
 echo -e "\e[1;32m设置完成\e[0m"
