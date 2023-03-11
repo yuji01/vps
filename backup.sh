61d889a13e689ffea89fb1a4e415ea3752265298
yuji01 <59014140+yuji01@users.noreply.github.com>
2023-03-11 17:46:13 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -20,6 +20,15 @@ copyfile(){
     [ -d $cloud_dir ] || mkdir $cloud_dir -p
     cp ${backup_path}/* $cloud_dir/ -r
 }
-
+notification_feishu(){
+    feishu_url="https://open.feishu.cn/open-apis/bot/v2/hook/741a60c0-78ee-4d49-bb14-xxxx"
+    if [ $? -eq 0 ];then
+curl -i -k  -H "Content-type: application/json" -X POST -d '{"msg_type":"'"text"'","content":{"text":"'"$file 备份成功！"'"}}' $feishu_url >/dev/null 2>&1
+    else
+curl -i -k  -H "Content-type: application/json" -X POST -d '{"msg_type":"'"text"'","content":{"text":"'"$file 备份失败！"'"}}' $feishu_url >/dev/null 2>&1
+    fi
+}
 file="bitwarden" && backup && copyfile
+notification_feishu
 file="freenom" && backup && copyfile
+notification_feishu
