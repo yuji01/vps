99c0ec2057079f8af278477f722f743c72b36d92
yuji01 <59014140+yuji01@users.noreply.github.com>
2023-03-12 13:37:15 +0800

    Update backup2.sh

--- backup2.sh
+++ backup2.sh
@@ -7,13 +7,18 @@ backup_name=`date  +"%Y-%m-%d_%H.%M.%S"`
 onedrive_path="/backup-vps/$1"
 
 feishu_url="https://open.feishu.cn/open-apis/bot/v2/hook/741a60c0-78ee-4d49-bb14-db4296595a6b"
+backup_file=$1
 success_info(){
-curl -i -k  -H "Content-type: application/json" -X POST -d '{"msg_type":"'"text"'","content":{"text":"'"$1 备份成功！"'"}}' $feishu_url >/dev/null 2>&1
+curl -i -k  -H "Content-type: application/json" -X POST -d '{"msg_type":"'"text"'","content":{"text":"'"$backup_file 备份成功！"'"}}' $feishu_url >/dev/null 2>&1
 }
 error_info(){
-curl -i -k  -H "Content-type: application/json" -X POST -d '{"msg_type":"'"text"'","content":{"text":"'"$1 备份失败！"'"}}' $feishu_url >/dev/null 2>&1
+curl -i -k  -H "Content-type: application/json" -X POST -d '{"msg_type":"'"text"'","content":{"text":"'"$backup_file 备份失败！"'"}}' $feishu_url >/dev/null 2>&1
 }
-[ -d onedrive_path ] || { error_info ; exit 1 }
+
+if [ ! -d $onedrive_path ] ;then
+    error_info && exit 1
+fi
+
 # 创建备份文件
 [ -d $backup_path ] || mkdir $backup_path -p
 # 删除7天前的文件
@@ -21,10 +26,11 @@ find $backup_path/* -type f -mtime +7 -exec sh rm -rf {} \;
 tar -zcf ${backup_path}/${backup_name}.tar.gz -C $src_dir $1
 
 # 复制备份文件到onerive
-[ -d onedrive_path ] || mkdir $onedrive_path -p
+[ -d $onedrive_path ] || mkdir $onedrive_path -p
 cp ${backup_path}/${backup_name}.tar.gz $onedrive_path/
 # 删除本机的备份文件
-if [ -e $onedrive_path/${backup_name}.tar.gz ];then
+
+if [ -e $onedrive_path/${backup_name}.tar.gz ]; then
     rm ${backup_path}/${backup_name}.tar.gz && success_info
 else
     error_info
