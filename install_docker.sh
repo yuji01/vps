04301bf4f546f4677f424bffe6a20fa978e3f21c
yuji01 <59014140+yuji01@users.noreply.github.com>
2023-03-16 10:09:18 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -36,8 +36,31 @@ case $os in
     yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine;
     yum install -y yum-utils &&
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
-    yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin    
+    yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 esac
 }
+change_config(){
+cat > /etc/docker/daemon.json <<\EOF
+{
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "20m",
+        "max-file": "3"
+    },
+    "ipv6": true,
+    "fixed-cidr-v6": "fd00:dead:beef:c0::/80",
+    "experimental":true,
+    "ip6tables":true
+}
+EOF
+}
+start_docker(){
+systemctl enable docker.socket
+systemctl enable docker
+systemctl restart docker
+}
 check_os
-install_docker && echo -e "\e[1;32mdocker 安装完成\e[0m" || echo -e "\e[1;31mdocker 安装失败\e[0m"
+install_docker &&
+change_config &&
+start_docker &&
+echo -e "\e[1;32mdocker 安装完成\e[0m" || echo -e "\e[1;31mdocker 安装失败\e[0m"
