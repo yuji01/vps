bd8fb7ee39ac53c832a567809195afd7fe217cc6
yuji01 <59014140+yuji01@users.noreply.github.com>
2023-11-08 20:27:35 +0800

    Update journalctl_size.sh

--- journalctl_size.sh
+++ journalctl_size.sh
@@ -2,6 +2,8 @@
 read -e -p "请输入日志的大小 单位M：" SIZE
 cat > /etc/systemd/journald.conf <<EOF
 SystemMaxUse=${SIZE}M
+RuntimeMaxUse=${SIZE}M
 EOF
+journalctl --vacuum-size=${SIZE}M
 echo "设置完成，journal日志大小为 ${SIZE}M"
 systemctl kill --kill-who=main --signal=SIGUSR2 systemd-journald.service
