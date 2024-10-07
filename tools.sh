d2fc8cdb40a5c06897ab96f252e625d8df46ac5f
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-07 12:01:32 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -69,7 +69,7 @@ bash <(curl -Ls https://raw.githubusercontent.com/emptysuns/Hi_Hysteria/main/ser
 }
 docker_install(){
 #安装官方docker
-bash <(curl https://raw.githubusercontent.com/yuji01/vps/main/install_docker.sh)
+bash <(curl https://raw.githubusercontent.com/yuji01/vps/refs/heads/main/install_docker/install_docker.sh)
 }
 xrayr(){
 #下载xrayr
