a44676101b5705cf61f44b82ba7f046ff7e8d715
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-07 15:52:12 +0800

    Update journalctl_size.sh

--- journalctl_size.sh
+++ journalctl_size.sh
@@ -1,12 +1,32 @@
 #! /bin/bash
 
-journalctl --disk-usage
-
-read -e -p "请输入日志的大小 单位M：" SIZE
+read -e -p "请输入 系统日志可以使用的最大磁盘空间 单位M：" SystemMaxUse
+read -e -p "请输入 单个日志文件的最大大小 单位M：" SystemMaxFileSize
+read -e -p "请输入 在内存中存储的临时日志的最大空间 单位M" RuntimeMaxUse
+read -e -p "请输入 单个临时日志文件的最大大小 单位M" RuntimeMaxFileSize
+read -e -p "是否清除所有日志？ Y/n：" user_input
 cat > /etc/systemd/journald.conf <<EOF
-SystemMaxUse=${SIZE}M
-RuntimeMaxUse=${SIZE}M
+# 设置系统日志可以使用的最大磁盘空间
+SystemMaxUse=${SystemMaxUse}M
+
+# 设置单个日志文件的最大大小
+SystemMaxFileSize=${SystemMaxFileSize}
+
+# 在内存中存储的临时日志的最大空间
+RuntimeMaxUse=${RuntimeMaxUse}M
+
+# 单个临时日志文件的最大大小
+RuntimeMaxFileSize=${RuntimeMaxFileSize}
+
 EOF
-journalctl --vacuum-size=${SIZE}M
-echo "设置完成，journal日志大小为 ${SIZE}M"
-systemctl kill --kill-who=main --signal=SIGUSR2 systemd-journald.service
+# journalctl --vacuum-size=${SIZE}M
+systemctl restart systemd-journald
+
+if [[ "$user_input" == "y" || "$user_input" == "Y" ]]; then
+    # 删除所有超过 1 秒的日志
+    journalctl --vacuum-time=1s
+fi
+
+echo "设置完成，当前日志使用情况： 
+$(journalctl --disk-usage)"
+#systemctl kill --kill-who=main --signal=SIGUSR2 systemd-journald.service
