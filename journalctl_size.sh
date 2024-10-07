1594478a98138b358deb8274ac7f3c068e78954b
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-07 15:56:49 +0800

    Update journalctl_size.sh

--- journalctl_size.sh
+++ journalctl_size.sh
@@ -2,19 +2,17 @@
 
 read -e -p "请输入 系统日志可以使用的最大磁盘空间 单位M：" SystemMaxUse
 read -e -p "请输入 单个日志文件的最大大小 单位M：" SystemMaxFileSize
-read -e -p "请输入 在内存中存储的临时日志的最大空间 单位M" RuntimeMaxUse
-read -e -p "请输入 单个临时日志文件的最大大小 单位M" RuntimeMaxFileSize
+read -e -p "请输入 在内存中存储的临时日志的最大空间 单位M：" RuntimeMaxUse
+read -e -p "请输入 单个临时日志文件的最大大小 单位M：" RuntimeMaxFileSize
 read -e -p "是否清除所有日志？ Y/n：" user_input
 cat > /etc/systemd/journald.conf <<EOF
+[Journal]
 # 设置系统日志可以使用的最大磁盘空间
 SystemMaxUse=${SystemMaxUse}M
-
 # 设置单个日志文件的最大大小
 SystemMaxFileSize=${SystemMaxFileSize}
-
 # 在内存中存储的临时日志的最大空间
 RuntimeMaxUse=${RuntimeMaxUse}M
-
 # 单个临时日志文件的最大大小
 RuntimeMaxFileSize=${RuntimeMaxFileSize}
 
