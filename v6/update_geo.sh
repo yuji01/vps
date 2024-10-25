df37678626193bfd0bb7303168a22305362a73ea
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-25 09:54:49 +0800

    Update update_geo.sh

--- v6/update_geo.sh
+++ v6/update_geo.sh
@@ -5,13 +5,14 @@
 cat > /etc/resolv.conf <<EOF
 nameserver 2001:67c:2b0::4
 nameserver 2001:67c:2b0::6
+nameserver 8.8.8.8
+nameserver 1.1.1.1
 EOF
 
-
-apt update
-apt install wget -y
+apt update > /dev/null 2>&1
+apt install wget -y > /dev/null 2>&1
 
 # 更新geoip文件
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O /etc/V2bX/geoip.dat
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O /etc/V2bX/geosite.dat
+wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O /etc/V2bX/geoip.dat
+wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O /etc/V2bX/geosite.dat
 echo "任务完成"
