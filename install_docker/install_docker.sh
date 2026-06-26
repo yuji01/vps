f8d254c42590666571ba2143ee9b06be4d3d9016
yuji01 <59014140+yuji01@users.noreply.github.com>
2026-06-27 00:07:12 +0800

    Update install_docker.sh

--- install_docker/install_docker.sh
+++ install_docker/install_docker.sh
@@ -15,13 +15,25 @@ check_os(){
 install_docker(){
 case $os in
   debian)
-    apt-get remove docker docker-engine docker.io containerd runc;
-    apt-get update &&
-    apt-get install -y ca-certificates curl gnupg lsb-release &&
-    mkdir -p /etc/apt/keyrings &&
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null &&
-    apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     ;;
   ubuntu)
     apt-get remove docker docker-engine docker.io containerd runc;
